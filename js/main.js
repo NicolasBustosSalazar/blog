3f86d41249852e8264f8da81de15801f9765f1dd
nick <nicobustos84@gmail.com>
2023-09-14 14:37:19 -0300

    visual changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }else{
-            entry.target.classList.remove('show');
-        }
-    });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
+const hiddenElements = document.querySelectorAll(".card");
 
+hiddenElements.forEach((el) => observer.observe(el));
 
+const newPost_btn = document.getElementById("NEW");
 
-
-const hiddenElements = document.querySelectorAll('.card');
-
-hiddenElements.forEach((el)=> observer.observe(el));
+newPost_btn.addEventListener("click", function (e) {
+  const name = prompt("nombre?");
+});
