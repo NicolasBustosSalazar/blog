5fe78805ede5e8d6ca295aa10716ba356b0514c8
nick <nicobustos84@gmail.com>
2023-09-17 19:46:50 -0300

    new post functionality added

--- js/main.js
+++ js/main.js
@@ -13,8 +13,42 @@ const hiddenElements = document.querySelectorAll(".card");
 
 hiddenElements.forEach((el) => observer.observe(el));
 
-const newPost_btn = document.getElementById("NEW");
+const post = document.getElementById("dialog");
+const boton = document.getElementById("NEW");
 
-newPost_btn.addEventListener("click", function (e) {
-  const name = prompt("nombre?");
+boton.addEventListener("click", () => {
+  post.showModal();
+});
+
+const guardar = document.getElementById("guardar");
+
+guardar.addEventListener("click", () => {
+  let divElement = document.createElement("div");
+  let post_name = document.getElementById("input").value;
+  let post_descrp = document.getElementById("descrip").value;
+  let post_photo = document.getElementById("photo").value;
+  //console.log(post_name + " hello world");
+  divElement.classList.add("card");
+  // divElement.classList.add("show");
+  let post =
+    `
+        <div class="post_header">
+          <img class="post_icon" src="` +
+    post_photo +
+    `" />
+          <span class="post_title">` +
+    post_name +
+    `</span>
+        </div>
+        <p>` +
+    post_descrp +
+    `
+        </p>`;
+  divElement.insertAdjacentHTML("beforeend", post);
+
+  let prime = document.getElementById("prime");
+  prime.appendChild(divElement);
+  const hiddenElements = document.querySelectorAll(".card");
+
+  hiddenElements.forEach((el) => observer.observe(el));
 });
